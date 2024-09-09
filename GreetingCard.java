b69e39e6d0a8bb0f24ba1ce748477f56e4bb4fac
GellyBird <147560865+GellyBird@users.noreply.github.com>
2024-09-09 15:58:48 +0000

    Finished Lab!!

---
+++ GreetingCard.java
@@ -0,0 +1,12 @@
+// India Jackson: This program welcomes the user to Brooklyn Tech and tells them my name and how old I am.
+
+public class GreetingCard {
+    public static void main(String[] args) {
+        System.out.print("Welcome to Brooklyn Tech!");
+        System.out.println(); // This blank line prints on the same line as "Welcome to Brooklyn Tech!"
+        System.out.println("My name is India");
+        System.out.print("I am ");
+        System.out.print(10+6);
+        System.out.println(" years old!");
+    }
+}
---
+++ Unit1Topic1.java
@@ -0,0 +1,9 @@
+public class Unit1Topic1 {
+    public static void main(String[] args) {
+        System.out.println(100 * 3 + 1);
+        System.out.println(30 + 3 - 2);
+        System.out.println(30/2);
+    }
+
+    
+}
